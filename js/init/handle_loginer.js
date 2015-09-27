f1819c6ba8085e22a303089de180b31dbb478466
xuezi <xzj15859722542@hotmail.com>
2015-09-27 14:28:50 +0800

    gaubee

--- js/init/handle_loginer.js
+++ js/init/handle_loginer.js
@@ -1,6 +1,7 @@
 //
 ;
-(window.coAjaxLoginUser = function(succ_cb) {
+(function() {
+
 	// alert("获取登陆者信息……"+location.pathname)
 
 	//获取登陆用户的信息
@@ -8,18 +9,22 @@
 	 * 管理员相关的页面无需登录
 	 */
 	var no_user_login_pages = {
-		"/cart.html": 1,
-		"/pay.html": 1
+		// "/cart.html": 1,
+		// "/pay.html": 1
 	};
 	if (no_user_login_pages[location.pathname]) {
 		return;
 	};
-	// alert("开始获取"+appConfig.user.loginer)
-	console.log("获取登陆者信息……", location.pathname);
+
 	/*
 	 * 获取登录者信息
 	 */
+	var _is_success, _is_ready, _is_send_coAjax;
+	console.log("获取登陆者信息……", location.pathname);
+
 	function _login_sucess(data) {
+		_is_ready = true;
+		_is_success = true;
 		// alert("success","用户登录成功");
 		console.log("登录者信息：", data.result);
 		userInfo = data.result;
@@ -30,6 +35,8 @@
 	};
 
 	function _login_err(errorCode) {
+		_is_ready = true;
+		_is_success = false;
 		// alert("用户未登录");
 		var must_login_pages = {
 			"/user/cart": 1,
@@ -47,6 +54,9 @@
 		}, _login_sucess, _login_err);
 	};
 
+	var succ_cb_list = []
+	var err_cb_list = []
+
 	window._wx_openid_login = function _wx_openid_login(openid) {
 		//自动登录
 		coAjax.get(appConfig.user.loginer, {
@@ -57,14 +67,37 @@
 		});
 	};
 
-	if (_isWX) {
-		//微信必须使用OPENID登录，而且不同站点必须独立授权，确保OPENID的独立性
-		globalGet("WEIXIN_OPENID:" + busInfo._id, function(openid) {
-			if (openid) {
-				_wx_openid_login(openid);
-			};
-		});
-	} else {
-		_cookie_login();
-	};
-})();
+	(window.coAjaxLoginUser = function(succ_cb, err_cb, _force_send) {
+		if (succ_cb instanceof Function) {
+			succ_cb_list.push(succ_cb);
+		}
+		if (err_cb instanceof Function) {
+			err_cb_list.push(err_cb);
+		}
+		if (_is_ready) {
+			var cb_list = _is_success ? succ_cb_list : err_cb_list;
+			for (var i = 0, len = cb_list.length; i < len; i += 1) {
+				var foo = cb_list[i];
+				foo();
+			}
+			cb_list.length = 0;
+			return;
+		}
+		if (_is_send_coAjax && !_force_send) {
+			return
+		}
+		_is_send_coAjax = true;
+
+
+		if (_isWX) {
+			//微信必须使用OPENID登录，而且不同站点必须独立授权，确保OPENID的独立性
+			globalGet("WEIXIN_OPENID:" + busInfo._id, function(openid) {
+				if (openid) {
+					_wx_openid_login(openid);
+				};
+			});
+		} else {
+			_cookie_login();
+		};
+	})();
+}());
